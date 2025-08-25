4a7bc796a613f09140ecf775b4f252ff1f5b8da5
Lakshay Lalia <145219264+lakshaylalia@users.noreply.github.com>
2025-08-26 00:30:20 +0530

    Added Lexer Analyzer files

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "frontend/lexer.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is from the main program" << endl;
+    Lexer mainLexer;
+    cout << "This is END" << endl;
+    return 0;
+}

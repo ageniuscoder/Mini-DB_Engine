cd1367e5270a7f9b5063c461643c995797725471
ageniuscoder <23bcs059@nith.ac.in>
2025-09-01 18:13:38 +0530

    added selectTable

--- src/AST/ASTNodes.h
+++ src/AST/ASTNodes.h
@@ -17,6 +17,40 @@ class CreateTable : public StatementNode{
         m_columns=columns;
     }
 
+    const string &getTableName(){
+        return m_tableName;
+    }
+
+    const vector<ColumnDefinition> &getColumns(){
+        return m_columns;
+    }
+
+};
+
+class SelectTable : public StatementNode {
+    private:
+    string m_tableName;
+    vector<string> m_columns;
+    unique_ptr<ExpressionNode> m_whereClause;
+
+    public:
+    SelectTable(const string &tableName,const vector<string> &columns, unique_ptr<ExpressionNode> whereClause){
+        m_tableName=tableName;
+        m_columns=columns;
+        m_whereClause=move(whereClause);
+    }
+
+    const string &getTableName(){
+        return m_tableName;
+    }
+
+    const vector<string> &getColumns(){
+        return m_columns;
+    }
+
+    const unique_ptr<ExpressionNode> &getWhereClause(){
+        return m_whereClause;
+    }
 };
 
 #endif
